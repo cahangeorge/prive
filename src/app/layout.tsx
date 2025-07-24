6d3b8a7159f3ea9825991ccc6357a330e5bdcbfc
GeZe <zoiagabriel28@gmail.com>
2025-07-24 14:48:51 +0300

    remove favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
     <html lang="ro" className="scroll-smooth">
       <head>
         <meta name="theme-color" content="#000000" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
       </head>
       <body
         className={`${montserrat.variable} ${playfairDisplay.variable} antialiased`}
